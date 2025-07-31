512c835ed4768c2c516692cca93736d310a31ac6
solarmove <vah9888@gmail.com>
2025-07-31 16:19:00 +0300

    refactor: comment out unused hierarchy level and position initialization logic

--- bot/services/startup.py
+++ bot/services/startup.py
@@ -4,26 +4,26 @@ from bot.utils.unitofwork import UnitOfWork
 
 async def on_startup():
     uow = UnitOfWork()
-    async with uow:
-        for level in range(1, 5):
-            level_exists = await uow.hierarchy_level_repo.find_one(level=level)
-            if not level_exists:
-                await uow.hierarchy_level_repo.add_one(
-                    data=dict(
-                        level=level,
-                        prompt="test prompt",
-                        analytics_prompt="test analytics prompt",
-                    )
-                )
-                await uow.commit()
-        for position, level in positions_map.items():
-            position_exists = await uow.positions.find_one(title=position)
-            if not position_exists:
-                position_model = await uow.hierarchy_level_repo.find_one(level=level)
-                await uow.positions.add_one(
-                    data=dict(
-                        title=position,
-                        hierarchy_level_id=position_model.id,
-                    )
-                )
-        await uow.commit()
+    # async with uow:
+    #     for level in range(1, 5):
+    #         level_exists = await uow.hierarchy_level_repo.find_one(level=level)
+    #         if not level_exists:
+    #             await uow.hierarchy_level_repo.add_one(
+    #                 data=dict(
+    #                     level=level,
+    #                     prompt="test prompt",
+    #                     analytics_prompt="test analytics prompt",
+    #                 )
+    #             )
+    #             await uow.commit()
+    #     for position, level in positions_map.items():
+    #         position_exists = await uow.positions.find_one(title=position)
+    #         if not position_exists:
+    #             position_model = await uow.hierarchy_level_repo.find_one(level=level)
+    #             await uow.positions.add_one(
+    #                 data=dict(
+    #                     title=position,
+    #                     hierarchy_level_id=position_model.id,
+    #                 )
+    #             )
+    #     await uow.commit()
