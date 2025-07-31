f05f7d0b0fd6a991563f1513380193e79f149060
solarmove <vah9888@gmail.com>
2025-07-31 15:57:20 +0300

    fix: correct alembic command execution in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@ set -e
 # Переходим в рабочую директорию (куда скопирован ваш код)
 cd /app
 
+
+
 # Применяем все миграции
-alembic upgrade head
+exec uv run alembic upgrade head
 
 # Запускаем бота
 exec uv run python -m bot
