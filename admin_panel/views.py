dc1e6f96b61d3374e4ffa56b281525b27610544a
solarmove <vah9888@gmail.com>
2025-08-06 14:04:03 +0300

    refactor: customize UserView with field exclusions and representation enhancements

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,3 +1,4 @@
+from starlette.requests import Request
 from starlette_admin.contrib.sqla import ModelView
 from bot.db.models.models import (
     User,
@@ -11,8 +12,27 @@ from bot.db.models.models import (
     TaskControlPoints,
 )
 
+
+class UserView(ModelView):
+    exclude_fields_from_list = [
+        "work_schedules",
+        "created_tasks",
+        "executed_tasks",
+        "reports",
+    ]
+    exclude_fields_from_detail = [
+        "work_schedules",
+        "created_tasks",
+        "executed_tasks",
+        "reports",
+    ]
+
+    async def repr(self, obj: User, request: Request) -> str:
+        return obj.full_name or obj.full_name_tg
+
+
 model_views = [
-    ModelView(User, icon="fa fa-user"),
+    UserView(User, icon="fa fa-user"),
     ModelView(HierarchyLevel, icon="fa fa-sitemap"),
     ModelView(Positions, icon="fa fa-briefcase"),
     ModelView(WorkSchedule, icon="fa fa-calendar"),
